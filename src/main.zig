68730c6270ec2811aa9f441e98d1a0e57a5aa23a
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-06-02 22:33:40 +0200

    Parse markdown snippets

--- src/main.zig
+++ src/main.zig
@@ -25,11 +25,25 @@ pub const Snippet = struct {
     startLine: usize,
     /// 0 indexed
     endLine: usize,
+
+    const Map = std.StringHashMap(Snippet);
 };
 
-const SnippetMap = std.StringHashMap(Snippet);
-fn parseSnippets(allocator: std.mem.Allocator, content: String, snippetStart: String, snippetEnd: String) !SnippetMap {
-    var result = SnippetMap.init(allocator);
+pub const MarkdownSnippet = struct {
+    name: String,
+    /// 0 indexed
+    startLine: usize,
+    /// 0 indexed
+    endLine: usize,
+
+    const List = std.ArrayList(MarkdownSnippet);
+    const openStartMarker = "<!-- snippet-start ";
+    const closeStartMarker = " -->";
+    const endMarker = "<!-- snippet-end -->";
+};
+
+fn parseSnippets(allocator: std.mem.Allocator, content: String, snippetStart: String, snippetEnd: String) !Snippet.Map {
+    var result = Snippet.Map.init(allocator);
     errdefer result.deinit();
     var start: ?struct {
         lineIndex: usize,
@@ -46,6 +60,27 @@ fn parseSnippets(allocator: std.mem.Allocator, content: String, snippetStart: St
             start = .{ .lineIndex = lineIndex, .name = name };
         } else if (start != null and std.mem.startsWith(u8, line, snippetEnd)) {
             try result.put(start.?.name, Snippet{ .startLine = start.?.lineIndex, .endLine = lineIndex });
+            start = null;
+        }
+    }
+    return result;
+}
+
+fn parseMarkdownSnippets(allocator: std.mem.Allocator, content: String) !MarkdownSnippet.List {
+    var result = MarkdownSnippet.List.init(allocator);
+    errdefer result.deinit();
+    var start: ?struct {
+        lineIndex: usize,
+        name: String,
+    } = null;
+    var lineIndex: usize = 0;
+    var lineIterator = std.mem.splitSequence(u8, content, "\n");
+    while (lineIterator.next()) |line| : (lineIndex += 1) {
+        if (std.mem.startsWith(u8, line, MarkdownSnippet.openStartMarker) and std.mem.endsWith(u8, line, MarkdownSnippet.closeStartMarker)) {
+            const name = line[MarkdownSnippet.openStartMarker.len .. line.len - MarkdownSnippet.closeStartMarker.len];
+            start = .{ .lineIndex = lineIndex, .name = name };
+        } else if (start != null and std.mem.eql(u8, MarkdownSnippet.endMarker, line)) {
+            try result.append(MarkdownSnippet{ .name = start.?.name, .startLine = start.?.lineIndex, .endLine = lineIndex });
             start = null;
         }
     }
@@ -120,13 +155,53 @@ test "Ignore dangling ends" {
     }, result);
 }
 
+test "Parse zero markdown snippet" {
+    const source =
+        \\x = 42
+    ;
+    var result = try parseMarkdownSnippets(std.testing.allocator, source);
+    defer result.deinit();
+    try std.testing.expectEqualDeep(&[_]MarkdownSnippet{}, result.items);
+}
+
+test "Parse one markdown snippet" {
+    const source =
+        \\<!-- snippet-start X -->
+        \\x = 42
+        \\<!-- snippet-end -->
+    ;
+    var result = try parseMarkdownSnippets(std.testing.allocator, source);
+    defer result.deinit();
+    try std.testing.expectEqualDeep(&[_]MarkdownSnippet{
+        .{ .name = "X", .startLine = 0, .endLine = 2 },
+    }, result.items);
+}
+
+test "Parse many markdown snippets" {
+    const source =
+        \\<!-- snippet-start X -->
+        \\x = 42
+        \\<!-- snippet-end -->
+        \\Not a snippet line
+        \\<!-- snippet-start Y -->
+        \\y = 0
+        \\<!-- snippet-end -->
+    ;
+    var result = try parseMarkdownSnippets(std.testing.allocator, source);
+    defer result.deinit();
+    try std.testing.expectEqualDeep(&[_]MarkdownSnippet{
+        .{ .name = "X", .startLine = 0, .endLine = 2 },
+        .{ .name = "Y", .startLine = 4, .endLine = 6 },
+    }, result.items);
+}
+
 const SnippetAssertItem = struct {
     name: String,
     snippet: Snippet,
 };
 
-fn expectSnippetsEquals(expected: []const SnippetAssertItem, actual: SnippetMap) !void {
-    var expectedMap = SnippetMap.init(std.testing.allocator);
+fn expectSnippetsEquals(expected: []const SnippetAssertItem, actual: Snippet.Map) !void {
+    var expectedMap = Snippet.Map.init(std.testing.allocator);
     defer expectedMap.deinit();
     try std.testing.expectEqual(expected.len, actual.count());
     for (expected) |expectedSnippet| {
